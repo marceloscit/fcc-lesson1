3ff6c25e271d407608316385719c8ba232b72a44
Marcelo Moreira <marcelo@scconsulting.com.br>
2022-05-27 10:30:59 +0000

    lesson 11

--- myApp.js
+++ myApp.js
@@ -3,20 +3,18 @@ const app = express();
 let helmet = require('helmet')
 var ninetyDaysInSeconds = 90*24*60*60;
 
-app.use(helmet.hidePoweredBy())
-app.use(helmet.frameguard({action: 'deny'}))
-app.use(helmet.xssFilter())
-app.use(helmet.noSniff())
-app.use(helmet.ieNoOpen())
-app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}) )
-app.use(helmet.dnsPrefetchControl())
-app.use( helmet.noCache())
-
-app.use(helmet.contentSecurityPolicy(
-  { directives: { defaultSrc: ["'self'"], 
-                 scriptSrc: ["'self'", "trusted-cdn.com"] 
-                }
-  } ))
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
 
 
 
