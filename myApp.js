bed2d99179b436bc2a98886a75858c68e156bdae
Marcelo Moreira <marcelo@scconsulting.com.br>
2022-05-27 10:25:14 +0000

    lesson 10

--- myApp.js
+++ myApp.js
@@ -12,7 +12,11 @@ app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}) )
 app.use(helmet.dnsPrefetchControl())
 app.use( helmet.noCache())
 
-
+app.use(helmet.contentSecurityPolicy(
+  { directives: { defaultSrc: ["'self'"], 
+                 scriptSrc: ["'self'", "trusted-cdn.com"] 
+                }
+  } ))
 
 
 
