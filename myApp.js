f595b894970cbfa9fe566bde95332100a381b752
Marcelo Moreira <marcelo@scconsulting.com.br>
2022-05-27 12:40:00 +0000

    lesson 12

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt'); 
 let helmet = require('helmet')
+
 var ninetyDaysInSeconds = 90*24*60*60;
 
 app.use(helmet({
